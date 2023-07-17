3a1a4b92c14303997ec5c12b0db85376673824f1
oscarg617 <oscargutierreza617@gmail.com>
2023-07-16 17:09:32 -0700

    reworked div structure for better alignment

--- src/components/guide/guide.component.jsx
+++ src/components/guide/guide.component.jsx
@@ -5,16 +5,14 @@ import './guide.styles.scss'
 
 const Guide = ({ directions }) => {
   return (
-    <Fragment>
-      <div className='guide-container'>
-        <h2>Features</h2>
-      </div>
+    <div className='guide-container'>
+      <h2>Features</h2>
       <div className='features-container'>
         {directions.map(direction => (
           <GuideItem key={direction.id} direction={direction} />
         ))}
       </div>
-    </Fragment>
+    </div>
   )
 }
 
